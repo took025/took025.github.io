92832370934d5b424a6be6aa3576e6d320014762
took025 <tokofxaladze@gmail.com>
2019-01-09 13:43:58 -0800

    script change

--- sec/js/script.js
+++ sec/js/script.js
@@ -1,23 +1,22 @@
-// // document.getElementById("burger");
-// // console.log(document.getElementById("burger"));
+// document.getElementById("burger");
+// console.log(document.getElementById("burger"));
 
-// $( document ).ready(function() {
-//   // Handler for .ready() called.
-//   console.log();
-//   $("#burger").on("click", function(){
-//   // 	console.log();
-//   // 	$("burger").addClass("active");
-//   // 	$("burger-menu").addClass("active");
-//   $(this).toggleClass("active");
-//   $("#burger-menu").toggleClass("active");
-//   })
-// });
-
-var burgerIcon = document.getElementById("burger");
-var burgerMenu = document.getElementById("burger-menu");
-
-burgerIcon.addEventListener("click",  function(){
-	console.log("click");
-	burgerIcon.classList.toggle("active")
-	burgerMenu.classList.toggle("active")
+$( document ).ready(function() {
+  // Handler for .ready() called.
+  console.log();
+  $("#burger").on("click", function(){
+  // 	console.log();
+  // 	$("burger").addClass("active");
+  // 	$("burger-menu").addClass("active");
+  $(this).toggleClass("active");
+  $("#burger-menu").toggleClass("active");
+  })
 });
+// var burgerIcon = document.getElementById("burger");
+// var burgerMenu = document.getElementById("burger-menu");
+
+// burgerIcon.addEventListener("click",  function(){
+// 	console.log("click");
+// 	burgerIcon.classList.toggle("active")
+// 	burgerMenu.classList.toggle("active")
+// });
