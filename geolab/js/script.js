f3ae661024c949526bb239b33b39f1bc0d24f616
took025 <tokofxaladze@gmail.com>
2019-01-10 15:47:53 -0800

    changes

--- geolab/js/script.js
+++ geolab/js/script.js
@@ -1,14 +1,25 @@
-// document.getElementById("burger");
-// console.log(document.getElementById("burger"));
 
-$( document ).ready(function() {
-  // Handler for .ready() called.
-  console.log();
-  $("#burger").on("click", function(){
-  // 	console.log();
-  // 	$("burger").addClass("active");
-  // 	$("burger-menu").addClass("active");
-  $(this).toggleClass("active");
-  $("#burger-menu").toggleClass("active");
-  })
+
+
+var burgerIcon = document.getElementById("burger");
+var burgerMenu = document.getElementById("burger-menu");
+
+burgerIcon.addEventListener("click",  function(){
+	console.log("click");
+	burgerIcon.classList.toggle("active")
+	burgerMenu.classList.toggle("active")
 });
+
+let anchorlinks = document.querySelectorAll('a[href^="#"]')
+
+for (let item of anchorlinks) { // relitere 
+    item.addEventListener('click', (e)=> {
+        let hashval = item.getAttribute('href')
+        let target = document.querySelector(hashval)
+        target.scrollIntoView({
+            behavior: 'smooth'
+        })
+        history.pushState(null, null, hashval)
+        e.preventDefault()
+    })
+}
