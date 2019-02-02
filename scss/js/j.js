4b5e216aa60d1a4451d00ac1c254a6ee8c1e0741
took025 <tokofxaladze@gmail.com>
2019-02-02 07:39:12 -0800

    n

--- scss/js/j.js
+++ scss/js/j.js
@@ -9,3 +9,50 @@ burgerIcon.addEventListener("click",  function(){
 	burgerIcon.classList.toggle("active")
 	burgerMenu.classList.toggle("active")
 });
+
+
+let anchorlinks = document.querySelectorAll('a[href^="#"]')
+
+for (let item of anchorlinks) { // relitere 
+    item.addEventListener('click', (e)=> {
+        let hashval = item.getAttribute('href')
+        let target = document.querySelector(hashval)
+        target.scrollIntoView({
+            behavior: 'smooth'
+        })
+        history.pushState(null, null, hashval)
+        e.preventDefault()
+    })
+};
+
+function myFunction() {
+	var element = document.getElementById("chat");
+	element.classList.toggle("active");
+ }
+
+ var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
